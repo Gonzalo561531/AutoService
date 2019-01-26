bb50507786ba93d0349dbc7e28e1decd9cca9b53
Gonzalo561531 <miki.gonzalo56@gmail.com>
2019-01-26 15:06:20 -0500

    Servicios para la base de datos Autos

---
+++ AutoServices/Controllers/AutoController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.DTOs;
+using Application.IServices;
+using AutoServices.Models.Repositories;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AutoServices.Controllers
+{
+    [Route("api/[controller]")]
+    public class AutoController : Controller
+    {
+        private IAutoService Service;
+        public AutoController(IAutoService service)
+        {
+            Service = service;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public IList<AutoDTO> Get()
+        {
+            return Service.GetAll();
+        }
+        // GET api/<controller>/5
+        [HttpGet("{AutoId}")]
+
+        public AutoDTO Get(Guid AutoId)
+        {
+
+            return Service.GetAll().Where(p => p.AutoId == AutoId).FirstOrDefault();
+        }
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]AutoDTO auto)
+        {
+            await Service.Insert(auto);
+            return Ok(true);
+        }
+        // PUT api/<controller>/5
+        [HttpPut("{AutoId}")]
+        public async Task<IActionResult> Put(Guid AutoId, [FromBody]AutoDTO auto)
+        {
+            auto.AutoId = AutoId;
+            await Service.Insert(auto);
+            return Ok(true);
+        }
+        // DELETE api/<controller>/5
+        [HttpDelete("{AutoId}")]
+        public IActionResult Delete(Guid AutoId)
+        {
+            Service.Delete(AutoId);
+            return Ok(true);
+        }
+    }
+}
---
+++ AutoServices/Controllers/MarcaController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.DTOs;
+using Application.IServices;
+using AutoServices.Models.Repositories;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AutoServices.Controllers
+{
+    [Route("api/[controller]")]
+    public class MarcaController : Controller
+    {
+        private IMarcaService Service;
+        public MarcaController(IMarcaService service)
+        {
+            Service = service;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public IList<MarcaDTO> Get()
+        {
+            return Service.GetAll();
+        }
+        // GET api/<controller>/5
+        [HttpGet("{MarcaId}")]
+        
+        public MarcaDTO Get(Guid MarcaId)
+        {
+
+            return Service.GetAll().Where(p => p.MarcaId == MarcaId).FirstOrDefault();
+        }
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]MarcaDTO marca)
+        {
+            await Service.Insert(marca);
+            return Ok(true);
+        }
+        // PUT api/<controller>/5
+        [HttpPut("{MarcaId}")]
+        public async Task<IActionResult> Put(Guid MarcaId, [FromBody]MarcaDTO marca)
+        {
+            marca.MarcaId = MarcaId;
+            await Service.Insert(marca);
+            return Ok(true);
+        }
+        // DELETE api/<controller>/5
+        [HttpDelete("{MarcaId}")]
+        public IActionResult Delete(Guid MarcaId)
+        {
+            Service.Delete(MarcaId);
+            return Ok(true);
+        }
+    }
+}
